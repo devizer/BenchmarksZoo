f92722bc8527fec581f8c0a69d7bb8fe1036cbab
devizer <devizer@users.github.com>
2019-12-16 00:51:00 +0200

    Added appveyor tests

--- run-llvm.sh
+++ run-llvm.sh
@@ -4,9 +4,9 @@ nuget restore || true; dotnet restore || true
 cd BenchmarksZoo
 msbuild /t:rebuild /p:Configuration=Release /v:q
 cd bin/Release/net47
-mono --llvm BenchmarksZoo.exe $1
 echo "ABOUT BenchmarksZoo.exe FILE:"
 ls -la BenchmarksZoo.exe
+mono --llvm BenchmarksZoo.exe $1
 cd ..
 cd ..
 
