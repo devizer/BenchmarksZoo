86360fe3c5d0baeab31a3e48a7e6e3d91f8e00c3
devizer <devizer@users.github.com>
2019-12-15 23:56:14 +0200

    added Classic QuickSort

--- BenchmarksZoo/BenchmarkRunnerProgram.cs
+++ BenchmarksZoo/BenchmarkRunnerProgram.cs
@@ -6,33 +6,38 @@ using BenchmarkDotNet.Environments;
 using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Reports;
 using BenchmarkDotNet.Running;
+using System.Linq;
 
 namespace BenchmarksZoo
 {
-    class BenchmarkRunnerProgram
+    public static class BenchmarkRunnerProgram
     {
+        private static bool IsRelease;
+
         static void Main(string[] args)
         {
-            var run = Job.ShortRun;
+            IsRelease = args.Any(x => x.IndexOf("release", StringComparison.InvariantCultureIgnoreCase) >= 0);
+            
+        
+            var run = IsRelease ? Job.MediumRun : Job.ShortRun;
             IConfig config = ManualConfig.Create(DefaultConfig.Instance);
             // Job jobLlvm = Job.InProcess;
 
             if (IsMono())
             {
-                Job jobLlvm = run.With(Jit.Llvm).With(MonoRuntime.Default).WithId("LLVM-ON").WithWarmupCount(3);
-                Job jobNoLlvm = run.With(MonoRuntime.Default).WithId("LLVM-OFF").WithWarmupCount(3);
+                Job jobLlvm = run.With(Jit.Llvm).With(MonoRuntime.Default).WithId("LLVM-ON").ConfigWarmUp();
+                Job jobNoLlvm = run.With(MonoRuntime.Default).WithId("LLVM-OFF").ConfigWarmUp();
                 config = config.With(new[] { jobLlvm, jobNoLlvm});
             }
 
-            Job jobCore22 = run.With(CoreRuntime.Core22).WithId("NET-CORE 2.2").WithWarmupCount(3);
-            Job jobCore30 = run.With(CoreRuntime.Core30).WithId("NET-CORE 3.0").WithWarmupCount(3);
-            Job jobCore31 = run.With(CoreRuntime.Core31).WithId("NET-CORE 3.1").WithWarmupCount(3);
+            Job jobCore22 = run.With(CoreRuntime.Core22).WithId("NET-CORE 2.2").ConfigWarmUp();
+            Job jobCore30 = run.With(CoreRuntime.Core30).WithId("NET-CORE 3.0").ConfigWarmUp();
+            Job jobCore31 = run.With(CoreRuntime.Core31).WithId("NET-CORE 3.1").ConfigWarmUp();
             config = config.With(new[] { jobCore22, jobCore30, jobCore31});
             
             if (Environment.OSVersion.Platform == PlatformID.Win32NT)
-                config = config.With(run.With(ClrRuntime.Net47).WithId("NETFW-47").WithWarmupCount(3));
+                config = config.With(run.With(ClrRuntime.Net47).WithId("NETFW-47").ConfigWarmUp());
                 
-            // Job jobFW47 = run.With(ClrRuntime.Net47).WithId("NETFW-47").WithWarmupCount(3);
             var summary = BenchmarkRunner.Run(typeof(BenchmarkRunnerProgram).Assembly, config);
             
         }
@@ -41,6 +46,13 @@ namespace BenchmarksZoo
         {
             return Type.GetType("Mono.Runtime", false) != null;
         }
+        
+        public static Job ConfigWarmUp(this Job job)
+        {
+            if (!IsRelease) job = job.WithWarmupCount(3);
+            return job;
+        }
+
 
     }
 }
