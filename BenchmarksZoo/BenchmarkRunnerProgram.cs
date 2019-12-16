cb9da75fd8164b34f22189c20d9662025b5aca00
devizer <devizer@users.github.com>
2019-12-16 15:41:37 +0200

    try to fix: Stack overflow in unmanaged: IP: 0x7f373b10530e, fault addr: 0x7fffcc410fe8             Stack overflow: IP: 0x55571736b47d, fault addr: 0x7fffcc408fa8             Stacktrace:               at <unknown> <0xffffffff>               at System.Globalization.CultureInfo.ConstructCurrentCulture () <0x00046>               <...>

--- BenchmarksZoo/BenchmarkRunnerProgram.cs
+++ BenchmarksZoo/BenchmarkRunnerProgram.cs
@@ -39,9 +39,16 @@ namespace BenchmarksZoo
 
             foreach (var jit in new[] {Jit.LegacyJit, Jit.RyuJit})
             {
-                Job jobCore22 = run.With(CoreRuntime.Core22).With(jit).WithId($"Net Core 2.2 {jit}").ConfigWarmUp();
-                Job jobCore30 = run.With(CoreRuntime.Core30).With(jit).WithId($"Net Core 3.0 {jit}").ConfigWarmUp();
-                Job jobCore31 = run.With(CoreRuntime.Core31).With(jit).WithId($"Net Core 3.1 {jit}").ConfigWarmUp();
+                Job jobCore22 = run.With(CoreRuntime.Core22).WithId($"Net Core 2.2 {jit}").ConfigWarmUp();
+                Job jobCore30 = run.With(CoreRuntime.Core30).WithId($"Net Core 3.0 {jit}").ConfigWarmUp();
+                Job jobCore31 = run.With(CoreRuntime.Core31).WithId($"Net Core 3.1 {jit}").ConfigWarmUp();
+                if (jit != Jit.RyuJit)
+                {
+                    jobCore22 = jobCore22.With(jit);
+                    jobCore30 = jobCore30.With(jit);
+                    jobCore31 = jobCore31.With(jit);
+                }
+
                 config = config.With(new[] { jobCore22, jobCore30, jobCore31});
             }
             
