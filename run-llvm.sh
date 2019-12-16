20a06b96f1b888b24559b736fe469a33a5417f58
devizer <devizer@users.github.com>
2019-12-16 13:02:59 +0200

    PLUS_AOT again

--- run-llvm.sh
+++ run-llvm.sh
@@ -32,7 +32,7 @@ list_for_aot='
 if [[ $"PLUS_AOT" ]]; then
     for to_aot in $list_for_aot; do
       echo "AOT: $to_aot"
-      time mono --aot=try-llvm -O=all "$to_aot" 
+      time sudo mono --aot=try-llvm -O=all "$to_aot" 
     done
     
     echo ""
