b6b307235ea3ecda5e07737e66bead22318d7cae
devizer <devizer@users.github.com>
2019-12-17 08:59:59 +0200

    Latency Benchmark again ***NO_CI***

--- BenchmarksZoo/SyncLatencyBenchmark.cs
+++ BenchmarksZoo/SyncLatencyBenchmark.cs
@@ -23,14 +23,33 @@ namespace BenchmarksZoo
             ThreadPool.QueueUserWorkItem(_ => done.Set());
             done.WaitOne();
         }
-        
+
+        [Benchmark]
+        public void ThreadLatencyWithJoin()
+        {
+            int y;
+            Thread t = new Thread(_ => y = 42);
+            t.Start();
+            t.Join();
+        }
+
+        [Benchmark]
+        public void ThreadLatencyWithWait()
+        {
+            ManualResetEvent done = new ManualResetEvent(false);
+            Thread t = new Thread(_ => done.Set());
+            t.Start();
+            done.WaitOne();
+            t.Join();
+        }
+
         [Benchmark]
         [Arguments(1)]
         [Arguments(2)]
         [Arguments(3)]
         [Arguments(8)]
         [Arguments(16)]
-        public void Benchmark(int racers)
+        public void CountdownLatency(int racers)
         {
             CountdownEvent race = new CountdownEvent(racers);
             for (int i = 0; i < racers; i++)
