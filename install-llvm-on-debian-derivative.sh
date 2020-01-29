fc4e076dc1500ca036106dee5f308f8363ce1528
devizer <devizer@users.github.com>
2020-01-29 08:51:20 +0200

    LLVM 10 & 11 on Bionic again

--- install-llvm-on-debian-derivative.sh
+++ install-llvm-on-debian-derivative.sh
@@ -3,9 +3,12 @@ v=${v:-9}
 echo Installing LLVM v${v}
 lsb_release || sudo apt-get install lsb-release -yqq
 code=$(lsb_release -c -s)
+echo ""; echo "DEBs"
 echo "
-deb http://apt.llvm.org/bionic/ llvm-toolchain-$code-$v main
-deb-src http://apt.llvm.org/bionic/ llvm-toolchain-$code-$v main
+deb http://apt.llvm.org/$code/ llvm-toolchain-$code main
+deb-src http://apt.llvm.org/$code/ llvm-toolchain-$code main
+deb http://apt.llvm.org/$code/ llvm-toolchain-$code-$v main
+deb-src http://apt.llvm.org/$code/ llvm-toolchain-$code-$v main
 " | sudo tee /etc/apt/sources.list.d/llvm.list
 sudo apt-get update -q
 apt-cache policy llvm
