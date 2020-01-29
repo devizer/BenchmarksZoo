ea6273349665bbb4a1477d76d968553587255990
devizer <devizer@users.github.com>
2020-01-29 09:08:58 +0200

    LLVM 10 & 11 on Bionic again 7

--- install-llvm-on-debian-derivative.sh
+++ install-llvm-on-debian-derivative.sh
@@ -14,6 +14,6 @@ sudo apt-get update -q
 apt-cache policy llvm
 sudo apt-get install clang-format clang-tidy clang-tools clang clangd libc++-dev libc++1 libc++abi-dev libc++abi1 libclang-dev libclang1 libllvm-ocaml-dev libomp-dev libomp5 lld lldb llvm-dev llvm-runtime llvm -yq
 sudo apt-get install -yq liblldb-dev python-clang
-llvm --version
+
 
 
