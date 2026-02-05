fcbb56d54883004290568a6965976636bf412098
devizer <devizer@users.github.io>
2026-02-05 23:09:58 +0200

    LockBenchmark.csproj 5.0 and 10.0 [skip ci]

--- LockBenchmark/RUN.ps1
+++ LockBenchmark/RUN.ps1
@@ -1,11 +1,12 @@
-dotnet build -c Release
 $netList=@("net10.0", "net5.0")
-if ($env:OS -eq "Windows_NT" -and $env:PROCESSOR_ARCHITECTURE -eq "ARM64") {
+if (-not ($env:OS -eq "Windows_NT" -and $env:PROCESSOR_ARCHITECTURE -eq "ARM64")) {
     $netList=@("netcoreapp3.1") + $netList
 }
 
-Write-Host "BENCHMARK RUNTIMES: [$netList]"
-sleep 2
+Write-Host "BENCHMARK RUNTIMES: [$netList]" -ForegroundColor Magenta
+sleep 3
+dotnet build -c Release -v:q /p:NoWarn="NETSDK1138"
+
 
 foreach($runtime in $netList) {
    Write-Host "runtime=[$runtime]" -ForegroundColor Magenta
